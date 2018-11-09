3690679a93ebd5171b4844e90ea24d05b649034a
k0t3n <k0t3n.mail@gmail.com>
2018-11-09 21:29:29 +0300

    Add currency app configuration

---
+++ src/apps/currency/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'src.apps.currency.apps.CurrencyConfig'
---
+++ src/apps/currency/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CurrencyConfig(AppConfig):
+    name = 'src.apps.currency'
+    verbose_name = 'Валюта'
