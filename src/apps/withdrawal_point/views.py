e4b351e4c2f50850922cb6bcd6fdcdb81787d5c1
k0t3n <k0t3n.mail@gmail.com>
2018-11-09 22:32:28 +0300

    Replace CustomQuerySet

--- src/apps/withdrawal_point/views.py
+++ src/apps/withdrawal_point/views.py
@@ -1,23 +1,8 @@
-from django.db import models
-from django.db.models import Case, When, F
 from rest_framework import viewsets
 
 from src.apps.withdrawal_point.filters import NearestItemsFilter, WithdrawalPointBanksFilter
 from src.apps.withdrawal_point.models import WithdrawalPoint
 from src.apps.withdrawal_point.serializers import WithdrawalPointSerializer
-
-
-class CustomQuerySet(models.QuerySet):
-    def annotate_with_custom_field(self):
-        return self.annotate(
-            custom_field=Case(
-                When(foo__isnull=False,
-                     then=F('foo__uuid')),
-                When(bar__isnull=False,
-                     then=F('bar__uuid')),
-                default=None,
-            ),
-        )
 
 
 class WithdrawalPointListView(viewsets.ReadOnlyModelViewSet):
