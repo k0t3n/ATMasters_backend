04c1f09825ec59a11b7362db4ae2412f8d77438a
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 00:44:44 +0300

    Add WithdrawalPointSimpleFieldsFilterSet to WithdrawalPointListView

--- src/apps/withdrawal_point/views.py
+++ src/apps/withdrawal_point/views.py
@@ -1,6 +1,8 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
 
-from src.apps.withdrawal_point.filters import NearestItemsFilter, WithdrawalPointBanksFilter
+from src.apps.withdrawal_point.filters import NearestItemsFilter, WithdrawalPointBanksFilter, \
+    WithdrawalPointSimpleFieldsFilterSet
 from src.apps.withdrawal_point.models import WithdrawalPoint
 from src.apps.withdrawal_point.serializers import WithdrawalPointSerializer
 
@@ -9,7 +11,9 @@ class WithdrawalPointListView(viewsets.ReadOnlyModelViewSet):
     queryset = WithdrawalPoint.objects.all()
     serializer_class = WithdrawalPointSerializer
     distance_filter_field = 'coordinates'
+    filterset_class = WithdrawalPointSimpleFieldsFilterSet
     filter_backends = (
         NearestItemsFilter,
         WithdrawalPointBanksFilter,
+        filters.DjangoFilterBackend,
     )
