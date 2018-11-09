a8fca082303bd4655fa844ee1f2f682a09ae3a79
k0t3n <k0t3n.mail@gmail.com>
2018-11-09 21:33:03 +0300

    Fix Bank model verbose names

--- src/apps/bank/models.py
+++ src/apps/bank/models.py
@@ -5,12 +5,12 @@ from django.utils.translation import ugettext_lazy as _
 class Bank(models.Model):
     title = models.CharField(
         max_length=255,
-        verbose_name=_('title'),
+        verbose_name=_('название'),
     )
 
     class Meta:
-        verbose_name = _('banks')
-        verbose_name_plural = _('bank')
+        verbose_name = _('банки')
+        verbose_name_plural = _('банк')
 
     def __str__(self):
         return self.title
