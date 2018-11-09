457244ec7779c13ea8ded46e3cfc70de1a967cd5
k0t3n <k0t3n.mail@gmail.com>
2018-11-09 21:27:21 +0300

    Add bank app configuration

---
+++ src/apps/bank/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'src.apps.bank.apps.BankConfig'
---
+++ src/apps/bank/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BankConfig(AppConfig):
+    name = 'src.apps.bank'
+    verbose_name = 'Банк'
