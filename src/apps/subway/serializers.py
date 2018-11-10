2f73e14886c75b832ce2069d5802aa62d7bfa6fc
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 14:12:39 +0300

    Add new fields to SubwayStationSerializer

--- src/apps/subway/serializers.py
+++ src/apps/subway/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from src.apps.subway.models import SubwayStation
+from src.apps.withdrawal_point.serializers import WithdrawalPointSerializer
 
 
 class SubwayStationSerializer(serializers.ModelSerializer):
+    nearest_withdrawal_points = WithdrawalPointSerializer(many=True)
+
     class Meta:
         model = SubwayStation
-        fields = '__all__'
+        fields = ('id', 'title', 'coords', 'branch_color', 'nearest_withdrawal_points')
