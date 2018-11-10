a842f3c93ba3e6eb8b3be2f2d5f25f14dc5dac76
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 13:43:07 +0300

    Replace withdrawal_point urls to root urls

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+
+from src.apps.bank.views import BankListView
+from src.apps.currency.views import CurrencyListView
+from src.apps.subway.views import SubwayStationsListView
+from src.apps.withdrawal_point.views import WithdrawalPointListView
+
+router = routers.SimpleRouter()
+
+router.register('withdrawalPoints', WithdrawalPointListView)
+router.register('banks', BankListView)
+router.register('currencies', CurrencyListView)
+router.register('subwayStations', SubwayStationsListView)
+
+api_urls = router.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/swagger', include('src.apps.swagger.urls')),
-    path('api/', include('src.apps.withdrawal_point.urls')),
+    path('api/', include(api_urls)),
 ]
