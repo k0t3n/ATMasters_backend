877866177a569422bd73558103ccc9127757dda5
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 23:14:20 +0300

    Update WithdrawalPointAdmin with django suit

--- src/apps/withdrawal_point/admin.py
+++ src/apps/withdrawal_point/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
 from django.contrib.gis.db import models
 from mapwidgets import GooglePointFieldWidget
+from suit.admin import RelatedFieldAdmin
 
 from .models import Schedule, WithdrawalPoint
 
 
-class WithdrawalPointAdmin(admin.ModelAdmin):
+@admin.register(WithdrawalPoint)
+class WithdrawalPointAdmin(RelatedFieldAdmin):
+
+    # TODO: fails on select_related, so we use code from admin.ModelAdmin instead
+    def get_queryset(self, request):
+        qs = super(admin.ModelAdmin, self).get_queryset(request)
+
+        return qs
+
     formfield_overrides = {
         models.PointField: {"widget": GooglePointFieldWidget}
     }
+    list_display = (
+        'point_type', 'bank', 'address', 'cash_in',
+        'cash_out', 'contactless_payments', 'mobile_payments',
+        'disabled_access', 'self_collection'
+    )
+    search_fields = ('address', 'point_type')
+    list_filter = ('point_type', 'bank', 'schedule')
+    list_select_related = True
+    filter_horizontal = ('schedule',)
 
 
 # TODO: admin
