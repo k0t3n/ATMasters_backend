bbf7285df5ab3e8c97a1e5f59e181a535d6acafc
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 18:34:20 +0300

    Add SubwayStationDetailedSerializer

--- src/apps/subway/serializers.py
+++ src/apps/subway/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+
 from src.apps.subway.models import SubwayStation
 from src.apps.withdrawal_point.serializers import WithdrawalPointSerializer
 
 
 class SubwayStationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubwayStation
+        fields = ('id', 'title', 'coords', 'branch_color',)
+
+
+class SubwayStationDetailedSerializer(serializers.ModelSerializer):
     nearest_withdrawal_points = WithdrawalPointSerializer(many=True)
 
     class Meta:
