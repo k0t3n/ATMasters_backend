d34d76f7547a55df3994f074d021434e5b444660
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 14:02:54 +0300

    Add SubwayStationAdmin

--- src/apps/subway/admin.py
+++ src/apps/subway/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis.db import models
+from mapwidgets import GooglePointFieldWidget
 
 from .models import SubwayStation
 
-# TODO: admin
-admin.site.register(SubwayStation)
+
+class SubwayStationAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.PointField: {"widget": GooglePointFieldWidget}
+    }
+
+
+admin.site.register(SubwayStation, SubwayStationAdmin)
