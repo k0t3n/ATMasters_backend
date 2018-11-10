59529c1e038a1abe9c4420b569b55d71127f96c5
k0t3n <k0t3n.mail@gmail.com>
2018-11-10 18:34:29 +0300

    Add get_serializer_class to SubwayStationsListView

--- src/apps/subway/views.py
+++ src/apps/subway/views.py
@@ -2,10 +2,15 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 
 from src.apps.subway.models import SubwayStation
-from src.apps.subway.serializers import SubwayStationSerializer
+from src.apps.subway.serializers import SubwayStationDetailedSerializer, SubwayStationSerializer
 
 
 class SubwayStationsListView(viewsets.ReadOnlyModelViewSet):
     queryset = SubwayStation.objects.all()
-    serializer_class = SubwayStationSerializer
     pagination_class = PageNumberPagination
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SubwayStationDetailedSerializer
+
+        return SubwayStationSerializer
